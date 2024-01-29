a17c950afb5b53c0d2f3e70b8dc9aa363da63f40
Isaiah <isaiahsimeone@gmail.com>
2024-01-30 00:14:23 +1000

    Refactored config stuff into config folder

--- run.py
+++ run.py
@@ -4,16 +4,15 @@ from messenger import Messenger
 from rustplus_api.rust_plus_api import RustPlusAPI
 from discord_bot.discord_bot import DiscordBot
 from web.web_server import WebServer
-from web.fcm_registration import FCMRegistration
-from config_manager import ConfigManager
+from config.config_manager import ConfigManager
 
 def main():
-    #config = ConfigManager("./config.json")   
-    config = None
-    messenger = Messenger(config)
+    config_manager = ConfigManager()   
+
+    messenger = Messenger(config_manager)
     
-    #if not config.get("fcm_credentials"):
-    FCMRegistration(messenger).register_with_fcm()
+    if not config.get("fcm_credentials"):
+        print("Need FCM credentials. Use rustplus companion chrome plugin")
    
     rustplus_api = RustPlusAPI(messenger)
     discord_bot = DiscordBot(messenger)
