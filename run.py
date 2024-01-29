14d7910825707ec53728c43cb1790da3d13b3229
Isaiah <isaiahsimeone@gmail.com>
2024-01-29 17:06:10 +1000

    Change dir names

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
 import asyncio
 from rustplus import RustSocket
 
+from rustplus_api import rust_plus_api
+from discord_bot import discord_bot
+from web import web_server
+
 def main():
     pass
 
