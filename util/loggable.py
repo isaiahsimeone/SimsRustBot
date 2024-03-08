fb4e264ba4067df016d42653ae1b4a2036b4f86a
Isaiah <isaiahsimeone@gmail.com>
2024-03-08 14:51:36 +1000

    Log fix

--- util/loggable.py
+++ util/loggable.py
@@ -2,8 +2,9 @@ class Loggable:
     def __init__(self, log_function):
         self.log_function = log_function
 
-    def log(self, *args, type="info"):
+    def log(self, *args, **kwargs):
         message = ' '.join(str(arg) for arg in args)
         prefix = self.__class__.__name__  # Get the class name as prefix
+        log_type = kwargs.get("type", "info")
         full_message = f"[{prefix}]: {message}"
-        self.log_function(full_message, type)
+        self.log_function(full_message, type=log_type)
